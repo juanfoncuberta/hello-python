76f993a90756034d035b4d33bc36f9686f415b34
Juan Foncuberta <juan.foncuberta@gmail.com>
2022-11-16 13:45:14 +0100

    Show data types

--- hello.py
+++ hello.py
@@ -1,4 +1,13 @@
 #hello word
-print('Hello world')
+print('Hello world') 
 
-print(Hello world)
+# Checking data types
+
+print(type('hello'))      # String
+print(type(10))          # Int
+print(type(3.14))        # Float
+print(type(1 + 3j))      # Complex number
+print(type([1, 2, 3]))   # List
+print(type({'name':'juan'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
