c6d59c72b016a369b4b6d908af89c44716771a05
Juan Foncuberta <juan.foncuberta@gmail.com>
2022-11-16 18:18:30 +0100

    Operators in python

---
+++ 02_operators.py
@@ -0,0 +1,53 @@
+# Operators
+
+
+print(3 + 4)    #Addition
+print(7 - 2)    #Subtraction
+print(7 * 2)    #Multiplication
+print(15 % 4)   #Modulus
+print(16 / 4)   #Division
+print(15 / 4)   #Division
+print(16 // 4)  #Division without the remainder
+print(15 // 4)  #Division without the remainder
+print(3 ** 3)   #Exponential
+
+
+
+
+
+#Operators comparison
+print(3 > 2)     # reater than 
+print(3 >= 2)    # greater than or equal
+print(2 < 3)     # less than
+print(2 <= 3)    # less than or equal
+print(3 == 2)    # equal
+print(3 != 2)    # not equal
+
+
+# Another way comparison 
+print('1 is 1', 1 is 1)                     #equal
+print('1 is 2', 1 is 2)                     #equal
+print('4 is 2 ** 2:', 4 is 2 ** 2)          #equal
+print('1 is not 2', 1 is not 2)             #not equal
+print('1 is not 1', 1 is not 1)             #not equal
+print('p in Python', 'p' in 'Python')       #Find in string case sensitive
+print('y in Python', 'y' in 'Python')       #Find in string case sensitive
+
+
+#Logic operators
+print(not 3 > 2)     # Negation
+print(not True)      # Negation
+print(not False)     # Negation
+print(not not True)  # Double negation
+print(not not False) # Double negation
+
+print('juan')
+print(True and True)  # And
+print(True & False)   # And
+print(True or False)  # Or
+print(False | False)  # Or
+
+
+
+# Concat strings
+print("Hello "+"World")
