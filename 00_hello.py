738dea8a2074028dc135618d13f2472455e76003
Juan Foncuberta <juan.foncuberta@gmail.com>
2022-11-16 14:00:23 +0100

    Naming files by step

---
+++ 00_hello.py
@@ -0,0 +1,25 @@
+#hello word
+print('Hello world') 
+
+# Checking data types
+
+print(type('hello'))      # String
+print(type(10))          # Int
+print(type(3.14))        # Float
+print(type(1 + 3j))      # Complex number
+print(type([1, 2, 3]))   # List
+print(type({'name':'juan'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+
+
+
+# Executing operations
+
+print(2 + 3)             # addition(+)
+print(3 - 1)             # subtraction(-)
+print(2 * 3)             # multiplication(*)
+print(3 / 2)             # division(/)
+print(3 ** 2)            # exponential(**)
+print(3 % 2)             # modulus(%)
+print(3 // 2)            # Floor division operator(//)
