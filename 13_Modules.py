c0105d8d96a9acb7cbe656968701eef85333256d
Juan Foncuberta <juan.foncuberta@gmail.com>
2023-01-08 16:20:49 +0100

    import modules

---
+++ 13_Modules.py
@@ -0,0 +1,12 @@
+## Modules ##
+
+#to import a module can't begin with a number. has to be snake_case
+import my_first_module
+from my_second_module import say_bye
+from math import pi as pi_value
+
+
+print(my_first_module.sum_val(2,3))
+my_first_module.say_hi()
+say_bye()
+print(pi_value)
---
+++ my_first_module.py
@@ -0,0 +1,5 @@
+def sum_val(val, val2):
+  return val + val2
+
+def say_hi():
+  print('Hi')
